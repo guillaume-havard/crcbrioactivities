1ceab65f7fa79ea9470e41579ee7848071bdcb80
Guillaume Havard <guill.havard@gmail.com>
2018-11-16 14:19:08 +0100

    [Dev] Get column id from header row
    Clean main file

--- crcbrioactivities.py
+++ crcbrioactivities.py
@@ -1,5 +1,7 @@
 import openpyxl
 
+#TODO: Add log system
+
 FILENAME = "tests/data/brio.xlsx"
 INITIAL_SHEET_NAME = "Sheet1"
 
@@ -8,17 +10,19 @@ if __name__ == "__main__":
     print(file.sheetnames)
     sheet = file[INITIAL_SHEET_NAME]
 
-    for row in sheet.rows:
-        for cell in row:
-            print(cell.value)
-
+    # Display sheet
     for row in sheet.iter_rows(min_row=1, max_row=1):
         for cell in row:
-            print(cell.value, end=" ")
+            print(cell.value, end=", ")
         print()
-
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
         for cell in row:
-            print(cell.value, end=" ")
+            print(cell.value, end=", ")
         print()
 
+    # TODO: get columns id from header
+    columns_id = {}
+    for row in sheet.iter_rows(min_row=1, max_row=1):
+        columns_id = {cell.value: id for id, cell in enumerate(row)}
+
+    print(columns_id)
